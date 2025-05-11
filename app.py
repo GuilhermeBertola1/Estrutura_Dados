d049e956d3925071040673b13cc14dae94caadc3
Guilherme Bertola <96829993+GuilhermeBertola1@users.noreply.github.com>
2025-05-11 14:53:36 -0300

    Comunicacao inicial por TCP
    Ajeitando a lib zeroMQ para c e criando um arquivo .py para ser o cliente do server criado por zeroMQ no C, ajeitando a comunicao entre as duas aplicacoes. Funcionando 100% por enquanto

---
+++ app.py
@@ -0,0 +1,21 @@
+"""
+Este é um cliente ZeroMQ que se conecta a um servidor e envia 10 solicitações,
+aguardando uma resposta após cada uma.
+"""
+import zmq
+
+context = zmq.Context()
+
+#  Socket to talk to server
+print("Connecting to hello world server…")
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:5555")
+
+#  Do 10 requests, waiting each time for a response
+for request in range(10):
+    print(f"Sending request {request} …")
+    socket.send(b"Hello")
+
+    #  Get the reply.
+    message = socket.recv()
+    print(f"Received reply {request} [ {message} ]")
--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <zmq.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <assert.h>
+#include <stdlib.h>
 #include "AVL/avl.h"
 
 int main() {
@@ -68,6 +70,19 @@ int main() {
     printf("\nDados ordenados por data-hora:\n");
     em_ordem(raiz);
 
-    getchar();
+    //  Socket to talk to clients
+    void *context = zmq_ctx_new ();
+    void *responder = zmq_socket (context, ZMQ_REP);
+    int rc = zmq_bind (responder, "tcp://*:5555");
+    assert (rc == 0);
+
+    while (1) {
+        char buffer [10];
+        zmq_recv (responder, buffer, 10, 0);
+        printf ("Received Hello\n");
+        sleep (1);          //  Do some 'work'
+        zmq_send (responder, "World", 5, 0);
+    }
+
     return 0;
 }
