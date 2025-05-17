d8108f498ad9c5c49b3a5bd54aa42b225c053036
Guilherme Bertola <96829993+GuilhermeBertola1@users.noreply.github.com>
2025-05-17 17:16:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,17 @@
+"""
+Este é um cliente ZeroMQ que se conecta a um servidor e envia 4300 solicitações,
+aguardando uma resposta após cada uma.
+"""
+
 import zmq
-import torch
 import streamlit as st
 
-# Título
-st.title("🛰️ Cliente ZeroMQ com PyTorch")
+st.title("🛰️ Cliente ZeroMQ")
 
-# GPU ou CPU
-if torch.cuda.is_available():
-    st.success("CUDA está disponível ✅")
-    st.write(f"Nome da GPU: {torch.cuda.get_device_name(0)}")
-    st.write(f"Memória total: {torch.cuda.get_device_properties(0).total_memory / (1024 ** 3):.2f} GB")
-else:
-    st.warning("CUDA não disponível. Usando CPU.")
-
-# Número de requisições
 num_reqs = st.number_input("Número de requisições a enviar", min_value=1, max_value=10000, value=10)
 
-# Botão para enviar requisições
 if st.button("Enviar requisições"):
+    
     st.write("🔌 Conectando ao servidor ZeroMQ...")
 
     context = zmq.Context()
