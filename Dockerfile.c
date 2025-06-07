d441f16b8d2d89d638739b36c328900aa2214b94
Guilherme Bertola <96829993+GuilhermeBertola1@users.noreply.github.com>
2025-06-07 12:17:48 -0300

    Update Dockerfile.c

--- Dockerfile.c
+++ Dockerfile.c
@@ -9,7 +9,7 @@ WORKDIR /app
 COPY main.c ./
 COPY AVL/ ./AVL
 COPY Cuckoo_Hashing/ ./Cuckoo_Hashing
-#COPY BM_tree/ ./BM_tree
+COPY BM_tree/ ./BM_tree
 COPY Hash_table/ ./Hash_table
 COPY Lista_Encadeada/ ./Lista_Encadeada
 COPY LSM_tree/ ./LSM_tree
